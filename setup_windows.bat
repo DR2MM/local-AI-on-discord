0e25ad5901343fc8e0403c4bab0f910509f2693e
DR2MM <omaroubik2014lmjrdin@gmail.com>
2026-03-11 13:53:32 +0000

    adding setup script

--- setup_windows.bat
+++ setup_windows.bat
@@ -2,10 +2,8 @@
 
 echo Creating virtual environment...
 python -m venv .venv
-echo Done.
 echo Activating environment...
 call .venv\Scripts\activate
-echo Done.
 echo Installing dependencies...
 pip install -r requirements.txt
 echo Done.
