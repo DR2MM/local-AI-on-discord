90df14694ebdcf33a6156cd5143b49521d48c046
DR2MM <omaroubik2014lmjrdin@gmail.com>
2026-03-11 13:54:16 +0000

    adding setup script

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,10 +2,8 @@
 
 echo "Creating environment..."
 python -m venv .venv
-echo "Done"
 echo "Activating environment..."
 source .venv/bin/activate
-echo "Done"
 echo "Installing packages..."
 pip install -r requirements.txt
 echo "Done"
